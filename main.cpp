a7da9169ff1cc6fcd329c70af1b3c02d653f8d7a
chiayuuu <xcvb880224@gmail.com>
2021-09-09 14:09:50 +0800

    加入註解

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)//註冊回調函數動態調整窗口大小
 {
     glViewport(0, 0, width, height);
 }
@@ -13,20 +13,20 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (key == GLFW_KEY_Q && action == GLFW_PRESS)
     {
         if (mods & GLFW_MOD_CONTROL) {
-            glfwSetWindowShouldClose(window, GLFW_TRUE);
+            glfwSetWindowShouldClose(window, GLFW_TRUE);//關閉窗口
         }
     }
 }
 
 int main()
 {
-    glfwInit();
+    glfwInit();//初始化
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);//寬,高,名稱 返回GLFWwindows對象
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -35,22 +35,22 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) { //初始化GLAD
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetKeyCallback(window, key_callback);
+    glViewport(150, 100, 800, 600);//窗口位置高,窗口位置寬,窗口渲染高,窗口渲染寬
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//註冊調整窗口函數
+    glfwSetKeyCallback(window, key_callback);//註冊手柄控制函數
 
     while(!glfwWindowShouldClose(window)) {
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwSwapBuffers(window);//交換顏色緩衝
+        glfwPollEvents();//檢查觸發事件 比如鍵盤滑鼠
     }
 
-    glfwTerminate();
+    glfwTerminate();//釋放and刪除分配資源
     return 0;
 }
