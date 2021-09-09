262d08e3a5654819feb412bdabc4d3b48b300743
chiayuuu <xcvb880224@gmail.com>
2021-09-09 17:27:03 +0800

    triangle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";//頂點著色器原碼
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";//片段著色器原始碼
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)//註冊回調函數動態調整窗口大小
 {
     glViewport(0, 0, width, height);
@@ -46,10 +61,10 @@ int main()
     }
 
     float vertices[] = {
-    -0.5f, -0.5f,  0.0f,
-     0.5f, -0.5f,  0.0f,
-     0.0f,  0.5f,  0.0f
-    };//三個頂點
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+    };
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);//生成VBO對象
@@ -59,19 +74,49 @@ int main()
     // GL_DYNAMIC_DRAW：數據會被改變。
      //GL_STREAM_DRAW ：數據每次都被改變。
 
-    const char* vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";//頂點著色器原碼
-    unsigned int vertexSharder;
-    vertexSharder = glCreateShader(GL_VERTEX_SHADER);
+    //創建和編譯著色器
+    //------------------------------------
+    //vertext shader 頂點渲染器
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);//將著色器原碼放進著色器對象內
+    glCompileShader(vertexShader);//編譯頂點著色器
+    //檢查著色器編譯是否有誤
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-    glShaderSource(vertexSharder, 1, &vertexShaderSource, NULL);//將著色器原碼放進著色器對象內
-    glCompileShader(vertexSharder);//編譯頂點著色器
+    //fragment shader 片段著色器
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);//將著色器原碼放進著色器對象內
+    glCompileShader(fragmentShader);//編譯頂點著色器
+     //檢查著色器編譯是否有誤
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
-    glViewport(150, 100, 800, 600);//窗口位置高,窗口位置寬,窗口渲染高,窗口渲染寬
+    //Shader Program著色器程序
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    //檢查著色器編譯是否有誤
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glViewport(100, 100, 800, 600);//窗口位置高,窗口位置寬,窗口渲染高,窗口渲染寬
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//註冊調整窗口函數
     glfwSetKeyCallback(window, key_callback);//註冊手柄控制函數
 
