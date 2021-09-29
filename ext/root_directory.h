2940d9a6bddcda73964eaf5d9837943edbea126a
kevin <kevin5881413kevin@gmail.com>
2021-09-29 16:44:49 +0800

    new

--- ext/root_directory.h
+++ ext/root_directory.h
@@ -1,1 +1,2 @@
-const char* logl_root = "C:/Users/lab503/Desktop/glfw-example--Source";
+//const char* logl_root = "C:/Users/lab503/Desktop/glfw-example--Source";
+const char* logl_root = "D:/Github/glfw_Test";
